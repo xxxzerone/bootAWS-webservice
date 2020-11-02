4c454a292eecca887d2ba252bde9150e10912790
KYW <testxxboy@naver.com>
2020-11-03 05:50:02 +0900

    Parameter get dto method add TDD test add

--- src/main/java/com/boot/book/springboot/web/HelloController.java
+++ src/main/java/com/boot/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.boot.book.springboot.web;
 
+import com.boot.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줍니다.
@@ -15,5 +17,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    /*
+        # @RequestParam
+        외부에서 API로 넘긴 파라미터를 가져오는 어노테이션입니다.
+        여기서는 외부에서 name (@RequestParam("name"))이란 이름으로 넘긴 파라미터를
+        메소드 파라미터 name(String name)에 저장하게 됩니다.
+     */
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }

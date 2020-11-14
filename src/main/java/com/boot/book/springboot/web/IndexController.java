d42bdf1a8301e19d28a950ac8f43ee68de9e1785
KYW <testxxboy@naver.com>
2020-11-14 21:09:33 +0900

    view board write add

--- src/main/java/com/boot/book/springboot/web/IndexController.java
+++ src/main/java/com/boot/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.boot.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }

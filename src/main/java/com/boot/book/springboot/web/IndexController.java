4bddd214d010cb239ddae823e6a759ddcc8c1a66
xshade <enpunk90@gmail.com>
2020-11-15 16:38:05 +0900

    OAuth2 google login add

--- src/main/java/com/boot/book/springboot/web/IndexController.java
+++ src/main/java/com/boot/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boot.book.springboot.web;
 
+import com.boot.book.springboot.config.auth.dto.SessionUser;
 import com.boot.book.springboot.service.posts.PostsService;
 import com.boot.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     /*
@@ -22,6 +26,20 @@ public class IndexController {
      */
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        /*
+            # (Session) httpSession.getAttribute("user")
+            * 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성했습니다.
+            * 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있습니다.
+         */
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        /*
+            # if(user != null)
+            * 세션에 저장된 값이 있을 때만 model에 userName으로 등록합니다.
+            * 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됩니다.
+         */
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 

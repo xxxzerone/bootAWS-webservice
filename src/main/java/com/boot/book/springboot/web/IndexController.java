f158fdac37019ab0d76a3776c18ba7c6b8b64c19
xshade <enpunk90@gmail.com>
2020-11-15 16:54:39 +0900

    HandlerMethodArgumentResolver using, Repeat code refactoring

--- src/main/java/com/boot/book/springboot/web/IndexController.java
+++ src/main/java/com/boot/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boot.book.springboot.web;
 
+import com.boot.book.springboot.config.auth.LoginUser;
 import com.boot.book.springboot.config.auth.dto.SessionUser;
 import com.boot.book.springboot.service.posts.PostsService;
 import com.boot.book.springboot.web.dto.PostsResponseDto;
@@ -23,15 +24,19 @@ public class IndexController {
         # Model
         * 서버 템플릿 엔진에서 사용할수 있는 객체를 저장할 수 있습니다.
         * 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달합니다.
+        # @LoginUser SessionUser user
+        * 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선 되었습니다.
+        * 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었습니다.
      */
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
         /*
             # (Session) httpSession.getAttribute("user")
             * 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성했습니다.
             * 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있습니다.
          */
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // @LoginUser 어노테이션으로 반복되는 부분 개선
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
         /*
             # if(user != null)
             * 세션에 저장된 값이 있을 때만 model에 userName으로 등록합니다.
